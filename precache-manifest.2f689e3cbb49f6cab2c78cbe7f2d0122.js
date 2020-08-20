d78358201522aea0f79c916795fb128992ee1583
tosadedapo <janewale@gmail.com>
2020-08-20 12:35:56 +0100

    Updates

---
+++ precache-manifest.2f689e3cbb49f6cab2c78cbe7f2d0122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4147299e28309b9a59ac991cb010b603",
+    "url": "/monsters-rollodex/index.html"
+  },
+  {
+    "revision": "9f18e8459cc000cd106d",
+    "url": "/monsters-rollodex/static/css/main.278cc41d.chunk.css"
+  },
+  {
+    "revision": "4438d3e118dd3662f387",
+    "url": "/monsters-rollodex/static/js/2.d0f53c86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rollodex/static/js/2.d0f53c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f18e8459cc000cd106d",
+    "url": "/monsters-rollodex/static/js/main.863a5fa1.chunk.js"
+  },
+  {
+    "revision": "490698ff30d612d77c36",
+    "url": "/monsters-rollodex/static/js/runtime-main.75bcd657.js"
+  }
+]);
